b7287af172a90b6c3ebf75e84455dc6560549c97
pludmann <p.ludmann@hotmail.fr>
2014-04-05 11:44:58 +0200

    Change from array type to list

--- sorting.ml
+++ sorting.ml
@@ -1,11 +1,11 @@
 let rec ratio_order (a1,b2,d1,c1,l1) (a2,b2,d2,c2,l2) =
 (l1/.c1)<=(l2/.c2);;
 
-rStreets = Sort.array ratio_order streets;;
+rStreets = Sort.list ratio_order streets;;
 
 
 let rec cost_order (a1,b2,d1,c1,l1) (a2,b2,d2,c2,l2) =
 (c1)<=(c2);;
 
-cStreets = Sort.array cost_order streets;;
+cStreets = Sort.list cost_order streets;;
 
